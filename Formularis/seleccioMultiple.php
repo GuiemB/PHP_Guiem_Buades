67b1287ae6fb5b1c9d6aa17fe019582802b1e4a2
GuiemB <guiem769@gmail.com>
2020-11-17 18:24:13 +0100

    Formulari

---
+++ Formularis/seleccioMultiple.php
@@ -0,0 +1,9 @@
+<?php
+    foreach ($_GET['nom'] as $opcio1)
+                echo "<b>El teu nom es:</b> $opcio1 </br>";
+    foreach ($_GET['email'] as $opcio2)
+                echo "<b>El teu email es:</b> $opcio2 </br>";
+    foreach ($_GET['navegadors'] as $opcio3)
+                echo "<b>El teu navegador es:</b> $opcio3 </br>";
+
+?>
